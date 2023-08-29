69545fcca3a870f16c064573031b5373e06ef031
sharadaade <aadesharad@gmail.com>
2023-08-29 04:09:57 -0500

    string

---
+++ 05_string.js
@@ -0,0 +1,33 @@
+// const name = "sharad"
+// const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// ************* String interpolation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('Aadesh')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(8));
+// console.log(gameName.indexOf('D'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const newString = "    sharad    "
+// console.log(newString);
+// console.log(newString.trim());
+
+// const url = "https://sharad.com/sharad%20aade"
+
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('aade'));
+// const st = new String('Aade Sharad Sahebrao')
+// console.log(st.split(' '));
+
+const st = new String('om-namha-shivay')
+console.log(st.split('-'));
---
+++ datatype_summery.js
@@ -0,0 +1,67 @@
+// Primitive
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLogIn = false
+// let outsideTemp = null
+// let userEmail;
+// console.log(typeof outsideTemp);
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+// const bigNumber = 242423543253453453n
+
+// Reference (Non primitive)
+// Array, Object, Function
+
+// ************************************** Array
+// const heros = ["shaktiman", "naagraj", "doga"]
+// let num = [2, 3, 4, 5]
+// console.log(typeof(heros));
+// console.log(typeof(num));
+
+// ************************************* Object
+// Inside curly braces treated as Object datatype
+// let myObj = {
+//     name: "sharad",
+//     age:24
+// }
+// console.log(typeof(myObj));
+
+// ************************************* Function 
+// const myFunction = function(){
+//     console.log("Hello");
+// }
+
+// console.log(typeof(myFunction));
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+// *************** Stack
+// let myYouTubename = "aadesh"
+
+// let anothername = myYouTubename
+// anothername = "chaiaurcode"
+
+// console.log(myYouTubename);
+// console.log(anothername);
+
+//***************** Heap
+// let userOne = {
+//     email:"aadesharad@gmail.com",
+//     upi:"aadesharad@ibl"
+// }
+
+// let userTwo = userOne
+
+// // userTwo.email = "aadesh@gmail.com"
+
+// console.log(userOne);
+// console.log(userTwo);
